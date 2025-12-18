7b2a2118bb83160cce8d533500bc097ad53b76c7
shrutirajput0904-sketch <shrutirajput0904@gmail.com>
2025-12-18 12:39:32 +0000

    basics

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+ "use strict"; // treat all js codes as newer version
+//  alert(3+3) // we are using nodejs, not browser
+// code readability should be high
+
+
+// number => 2 to the power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// underfined 
+// sumbol => unique
+
+// object 
+
+console.log(typeof "shruti" );  // it tells us about the type of data
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
