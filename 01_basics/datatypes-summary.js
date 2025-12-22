84319766a708d68690156edb942c54eee4b61633
shrutirajput0904-sketch <shrutirajput0904@gmail.com>
2025-12-22 13:13:13 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+// primitive (call by value)
+
+// 7 types: string, Number, Boolean, Null, undefined, symbol, BigInt
+
+// non-primitive (call by reference)
+
+// array, objects, functions
+
+// javascript is a dynamic language.
+
