5564c7a3caad35785ebd2900c5164d4e26ca6094
shrutirajput0904-sketch <shrutirajput0904@gmail.com>
2025-12-22 13:22:53 +0000

    memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,10 +1,16 @@
-// primitive (call by value)
+// primitive (call by value => changes to copies,doesnot affects the original.)
 
 // 7 types: string, Number, Boolean, Null, undefined, symbol, BigInt
 
-// non-primitive (call by reference)
+// non-primitive (call by reference => changes the value through another reference, affects the original.)
 
 // array, objects, functions
 
 // javascript is a dynamic language.
+
+
+// MEMORY
+
+
+// stack (primitve), heap(non-primitve)
 
